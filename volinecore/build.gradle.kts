d888441ac79f4ac84037e6c7c1a07b827c1ec9e5
Nitesh <niteshgupta288@gmail.com>
2026-05-30 23:10:01 +0530

    no libs version toml usage in module build gradle

--- volinecore/build.gradle.kts
+++ volinecore/build.gradle.kts
@@ -1,55 +1,56 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-	alias(libs.plugins.android.library)
-	alias(libs.plugins.ksp)
+    id("com.android.library")
+    id("com.google.devtools.ksp")
 }
 
 android {
-	namespace = "com.cropintellix.volinecore"
-	compileSdk {
-		version = release(36)
-	}
+    namespace = "com.cropintellix.volinecore"
+    compileSdk {
+        version = release(36)
+    }
 
-	defaultConfig {
-		minSdk = 24
+    defaultConfig {
+        minSdk = 24
 
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-		consumerProguardFiles("consumer-rules.pro")
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_17
-		targetCompatibility = JavaVersion.VERSION_17
-	}
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
 }
 
 kotlin {
-	compilerOptions {
-		jvmTarget = JvmTarget.JVM_17
-	}
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
 }
 
 dependencies {
-	implementation(libs.androidx.appcompat)
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.material)
-	testImplementation(libs.junit)
-	androidTestImplementation(libs.androidx.espresso.core)
-	androidTestImplementation(libs.androidx.junit)
+    //noinspection UseTomlInstead
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.core:core-ktx:1.18.0")
+    implementation("com.google.android.material:material:1.14.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
 
-	// Firebase
-	api(platform(libs.firebase.bom))
-	api(libs.firebase.database.ktx)
+    // Firebase
+    api(platform("com.google.firebase:firebase-bom:34.14.0"))
+    api("com.google.firebase:firebase-database")
 
-	// Room (offline log queue)
-	implementation(libs.room.runtime)
-	implementation(libs.room.ktx)
-	ksp(libs.room.compiler)
+    // Room (offline log queue)
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
+    ksp("androidx.room:room-compiler:2.8.4")
 
-	// Gson (JSON serialization)
-	implementation(libs.gson)
+    // Gson (JSON serialization)
+    implementation("com.google.code.gson:gson:2.14.0")
 
-	// Coroutines
-	implementation(libs.coroutines.android)
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
 }
